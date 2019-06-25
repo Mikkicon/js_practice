7b93f74ae4f5a732f68c156477395393773ea2f6
mikkicon <mikkicon@gmail.com>
2019-06-25 20:02:12 +0300

    +conditional check on every iteration of while loop; \n -dublicate while loops

--- JS/Task1/fibonacci.js
+++ JS/Task1/fibonacci.js
@@ -7,26 +7,16 @@ function* fibo({ back }) {
     let nMin1 = 1;
     let nMin2 = 0;
     let tempResult = 0;
-    if (back) {
-        while (1) {
-            yield tempResult;
-            tempResult = nMin2 - nMin1
-            nMin2 = nMin1;
-            nMin1 = tempResult;
-        }
-    } else {
-        // I understand that dublicate code is a bad tone, 
-        // however in this case it is better then 
-        // conditional on every iteration of while true
-        while (1) {
-            yield tempResult;
-            tempResult = nMin2 + nMin1
-            nMin2 = nMin1;
-            nMin1 = tempResult;
-        }
+    while (1) {
+        yield tempResult;
+        if (back) { tempResult = nMin2 - nMin1 } else { tempResult = nMin2 + nMin1 }
+        nMin2 = nMin1;
+        nMin1 = tempResult;
     }
 }
+
 const gen = fibo({ back: answer });
+
 while (amount) {
     for (let i = 0; i < amount; i++) {
         console.log(gen.next().value);
